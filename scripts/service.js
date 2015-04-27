9ee23cf168c16d74fc0db4bb825bfc7422f9a0ff
Brent Haas <brenthaas@gmail.com>
2015-04-27 12:30:54 -0700

    added loading screen and signupYear

--- scripts/service.js
+++ scripts/service.js
@@ -7,10 +7,12 @@ angular.module('twitterscore')
       return $http.get(scoreUrl + handle + "/profile").then(
         function(data){
           var resp = data.data;
+          var signupDate = new Date(resp.created_at)
           return {
             name: resp.name,
             handle: resp.screen_name,
             location: resp.location,
+            signupYear: signupDate.getFullYear(),
             following: resp.friends_count,
             followers: resp.followers_count
           }
